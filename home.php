f8faae9b55ad629f462c57491fdb45a0ebb8e7ed
Sam-Jab <salah8jab@gmail.com>
2024-03-14 22:53:20 +0000

    first commit

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="">
+  	<meta name="description" content="">
+    <!-- stylesheets css -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<link rel="stylesheet" href="css/nivo-lightbox.css">
+<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+
+<link rel="stylesheet" href="css/hover-min.css">
+<link rel="stylesheet" href="css/flexslider.css">
+
+<link rel="stylesheet" href="css/style.css">
+
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600' rel='stylesheet' type='text/css'>
+    <title>FST Gaming Club </title>
+</head>
+
+<body>
+    
+</body>
+</html>
