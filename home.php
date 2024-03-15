b7d2344c85bed4dcd11d392f19a1c4f9b9c1c27f
SamirTaous <74669151+SamirTaous@users.noreply.github.com>
2024-03-15 23:45:58 +0000

    fixed bootstrap-version

--- home.php
+++ home.php
@@ -15,8 +15,8 @@
   </script>
   <!-- stylesheets css -->
 
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/magnific-popup.css">
 
   <link rel="stylesheet" href="css/animate.min.css">
@@ -28,8 +28,6 @@
   <link rel="stylesheet" href="css/hover-min.css">
   <link rel="stylesheet" href="css/flexslider.css">
 
-  <link rel="stylesheet" href="css/styles.css">
-
   <link href='https://fonts.googleapis.com/css?family=Fjalla+One&family=Angkor&&family=Montserrat:400,700' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600' rel='stylesheet' type='text/css'>
   <link href="https://fonts.cdnfonts.com/css/minecraft-4" rel="stylesheet">
@@ -37,6 +35,9 @@
 </head>
 
 <body>
+
+  <!-- home -->
+
   <section id="home" class="parallax-section">
     <div class="container-fluid">
       <div class="backgrounds-container">
@@ -53,7 +54,37 @@
     </div>
   </section>
 
+  <!-- navbar -->
 
+  <section id="navigation">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <a class="navbar-brand" href="">FSTT</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#homenavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="homenavbar" class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link smoothScroll" href="#">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link smoothScroll" href="#">What we offer</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </section>
   <!-- javscript js -->
   <script src="js/main.js"></script>
   <script src="js/jquery.js"></script>
@@ -61,8 +92,8 @@
   <script src="js/jquery.parallax.js"></script>
   <script src="js/smoothscroll.js"></script>
   <script src="js/wow.min.js"></script>
-
   <script src="js/custom.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
