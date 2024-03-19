5c5162f270d4da41df6def7037d575d1181378e7
SamirTaous <74669151+SamirTaous@users.noreply.github.com>
2024-03-19 13:34:54 +0000

    third commit

--- signup.php
+++ signup.php
@@ -1,15 +1,20 @@
-<?php 
-session_start(); 
-include ("db_conn.php");
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+include("db_conn.php");
 
-if (isset($_POST['user_name']) && isset($_POST['password'])
-    && isset($_POST['user_email']) && isset($_POST['tel'])) {
+if (
+	isset($_POST['user_name']) && isset($_POST['password'])
+	&& isset($_POST['user_email']) && isset($_POST['tel'])
+) {
 
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
+	function validate($data)
+	{
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
 	}
 
 	$user_name = validate($_POST['user_name']);
@@ -22,46 +27,41 @@ if (isset($_POST['user_name']) && isset($_POST['password'])
 
 
 	if (empty($user_name)) {
-		header("Location: index.php?error_signup=User Name is required");
-	    exit();
-	}else if(empty($password)){
-        header("Location: index.php?error_signup=Password is required");
-	    exit();
-	}
-	else if(empty($user_email)){
-        header("Location: index.php?error_signup=Email is required");
-	    exit();
-	}
-
-	else if(empty($tel)){
-        header("Location: index.php?error_signup=Phone Number is required");
-	    exit();
-	}
-	else{
+		header("Location: loginscreen.php?error_signup=User Name is required");
+		exit();
+	} else if (empty($password)) {
+		header("Location: loginscreen.php?error_signup=Password is required");
+		exit();
+	} else if (empty($user_email)) {
+		header("Location: loginscreen.php?error_signup=Email is required");
+		exit();
+	} else if (empty($tel)) {
+		header("Location: loginscreen.php?error_signup=Phone Number is required");
+		exit();
+	} else {
 
 		// hashing the password
-        $password = md5($password);
+		// $password = md5($password);
 
-	    $sql = "SELECT * FROM users WHERE user_name='$user_name' ";
+		$sql = "SELECT * FROM users WHERE user_name='$user_name' ";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
-			header("Location: index.php?error_signup=The username is taken try another one ");
-	        exit();
-		}else {
-           $sql2 = "INSERT INTO users(user_name, user_email , password, tel) VALUES('$user_name', '$user_email', '$password' , '$tel')";
-           $result2 = mysqli_query($conn, $sql2);
-           if ($result2) {
-           	 header("Location: index.php?success_signup=Your account has been created successfully");
-	         exit();
-           }else {
-	           	header("Location: signup.php?error_signup=unknown error occurred");
-		        exit();
-           }
+			header("Location: loginscreen.php?error_signup=The username is taken try another one ");
+			exit();
+		} else {
+			$sql2 = "INSERT INTO users(user_name, user_email , password, tel) VALUES('$user_name', '$user_email', '$password' , '$tel')";
+			$result2 = mysqli_query($conn, $sql2);
+			if ($result2) {
+				header("Location: loginscreen.php?success_signup=Your account has been created successfully");
+				exit();
+			} else {
+				header("Location: signup.php?error_signup=unknown error occurred");
+				exit();
+			}
 		}
 	}
-	
-}else{
-	header("Location: index.php");
+} else {
+	header("Location: loginscreen.php");
 	exit();
 }
